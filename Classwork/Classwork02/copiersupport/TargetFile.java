a68e451d93d5571af4302016ce1efa35ae90437a
Nicolas Raymundo <nico@Nicolass-MacBook-Pro.local>
2018-09-06 16:39:08 -0700

    Added BufferedWriter to write contents of string to new file.

--- Classwork/Classwork02/copiersupport/TargetFile.java
+++ Classwork/Classwork02/copiersupport/TargetFile.java
@@ -1,7 +1,23 @@
+/**
+ *  File name     :  TargetFile.java
+ *  Purpose       :  Takes string from MyFileCopier and writes contents to a new file.
+ *  Author        :  Nicolas Raymundo
+ *  Date          :  2018-09-06
+ *  Notes         :  Uses FileWriter and BufferedWriter classes to take the string and copy it to a new file.
+ */
+
 package copiersupport;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TargetFile {
-    public TargetFile(String fileName, String newFile) {
-        super();
+
+    public TargetFile(String fileContents, String newFile) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(newFile));
+            writer.write(fileContents);
+        } catch (IOException ioe) {
+        }
     }
 }
