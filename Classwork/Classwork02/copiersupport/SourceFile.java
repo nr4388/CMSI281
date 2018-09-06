abf6a42e7ab948481bd64c1c8ce814cc59040988
Nicolas Raymundo <nico@Nicolass-MacBook-Pro.local>
2018-09-06 16:38:12 -0700

    Added bufferedReader to read contents and assign to new string.

--- Classwork/Classwork02/copiersupport/SourceFile.java
+++ Classwork/Classwork02/copiersupport/SourceFile.java
@@ -1,10 +1,25 @@
+/**
+ *  File name     :  SourceFile.java
+ *  Purpose       :  Takes file from MyFileCopier and reads contents.
+ *  Author        :  Nicolas Raymundo
+ *  Date          :  2018-09-06
+ *  Notes         :  Uses FileReader and BufferedReader classes to take a user's file and read it.
+ */
+
 package copiersupport;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class SourceFile {
     private String newFile;
 
     public SourceFile(String fileName) {
-        super();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            newFile = bufferedReader.readLine();
+        } catch (IOException ioe){
+        }
     }
 
     public String getContents() {
