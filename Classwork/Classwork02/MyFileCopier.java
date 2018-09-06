5223788639855022ae25a0c310d8ae8aa7ccd01b
Nicolas Raymundo <nico@Nicolass-MacBook-Pro.local>
2018-09-06 15:08:37 -0700

    called getContents on SourceFile and added TargetFile constructor

--- Classwork/Classwork02/MyFileCopier.java
+++ Classwork/Classwork02/MyFileCopier.java
@@ -13,16 +13,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-import copiersupport.*;
+import copiersupport.SourceFile;
+import copiersupport.TargetFile;
 
 public class MyFileCopier {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
         System.out.println("What file would you like to copy?");
         Scanner fileName = new Scanner( System.in );
         String inputName = fileName.nextLine();
         SourceFile sf = new SourceFile(inputName);
-        // String s = sf.get();
-        // TargetFile tf = new TargetFile(s, inputName + ".copy");
+        String s = sf.getContents();
+        TargetFile tf = new TargetFile(s, inputName + ".copy");
     }
 
 }
