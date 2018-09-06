314223a538b94824b0792c72f243bff87b269977
Nicolas Raymundo <nico@Nicolass-MacBook-Pro.local>
2018-09-06 15:07:16 -0700

    Added getContents method

--- Classwork/Classwork02/copiersupport/SourceFile.java
+++ Classwork/Classwork02/copiersupport/SourceFile.java
@@ -1,7 +1,14 @@
 package copiersupport;
 
 public class SourceFile {
+    private String newFile;
+
     public SourceFile(String fileName) {
         super();
     }
+
+    public String getContents() {
+        return newFile;
+    }
+
 }
