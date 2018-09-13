eee57d780aebc11c02e056dc23f59d90088f61c7
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-09-13 16:00:25 -0700

    Working except for error with insertValueAtIndex

--- Classwork/Classwork03/IntList.java
+++ Classwork/Classwork03/IntList.java
@@ -1,5 +1,6 @@
 public class IntList implements IntListInterface {
    private int[] theList;
+   private int[] newList;
    private int   size;
 
    private static final int STARTING_SIZE = 19;    // defines starting size; don't worry, we'll deal
@@ -10,31 +11,29 @@ public class IntList implements IntListInterface {
    }
 
    public boolean prepend( int valueToAdd ) {
-       expandArray(0);
-       theList[0] = valueToAdd;
+       expandArray();
+       size++;
+       insertValueAtIndex(valueToAdd, 0);
        return true;
    }
 
    public int getValueAtIndex( int index ) {
-      return checkIndex(index);
+      checkIndex(index);
+      return theList[index];
    }
 
    public boolean append( int valueToAdd ) {
-      if( size < theList.length ) {
-         theList[size] = valueToAdd;
-         size++;
-         return true;
-      } else {
-         // what should we do here, if there's no room?
-      }
-      return false;
+      expandArray();
+      theList[size] = valueToAdd;
+      size++;
+      return true;
    }
 
-   public void expandArray(int index) {
-       if (index > theList.length) {
-           int[] newList = new int[theList.length + STARTING_SIZE];
+   public void expandArray() {
+       if (size >= theList.length) {
+           newList = new int[theList.length + STARTING_SIZE];
            for (int i = 0; i < theList.length - 1; i++) {
-               theList[i] = newList[i];
+               newList[i] = theList[i];
            }
            theList = newList;
        }
@@ -42,37 +41,36 @@ public class IntList implements IntListInterface {
 
    // we've gotta have this to actually get things to compile
    public boolean insertValueAtIndex( int value, int index ) {
-       expandArray(index);
-       for (int i = theList.length; i > index; i--) {
-           theList[i] = theList[i-1];
+       try {
+           for (int i = theList.length - 1; i > index; i--) {
+               System.out.println("i: " + i);
+               System.out.println("list[i]: " + theList[i]);
+               System.out.println("index: " + index);
+               System.out.println("theList[i+1]" + theList[i + 1]);
+               theList[i + 1] = theList[i];
+           }
+           theList[index] = value;
+       } catch(ArrayIndexOutOfBoundsException a) {
+           System.out.println("Index is not valid");
        }
-       theList[index] = value;
+       // System.out.println("value at index: " + theList[index]);
        return true;
    }
 
-   public int checkIndex(int index) throws ArrayIndexOutOfBoundsException {
+   public void checkIndex(int index) throws ArrayIndexOutOfBoundsException {
        if (size == 0) {
            throw new ArrayIndexOutOfBoundsException( "The list is empty!" );
        } else if (index > size) {
            throw new ArrayIndexOutOfBoundsException( "The index value is too large" );
        } else if (index < 0) {
            throw new ArrayIndexOutOfBoundsException( "The index value is too small");
-       } else {
-           return theList[index];
        }
    }
 
    public int removeValueAtIndex( int index ) throws ArrayIndexOutOfBoundsException {
       int value = theList[index];
-      if( size == 0 ) {
-         throw new ArrayIndexOutOfBoundsException( "The list is empty!" );   // maybe not the best...
-      } else if( index > size ) {
-         throw new ArrayIndexOutOfBoundsException( "The index value is too large" );
-      } else if( index < 0 ) {
-         throw new ArrayIndexOutOfBoundsException( "The index value is too small");
-      } else {
-         holeFiller( index );
-      }
+      checkIndex(index);
+      holeFiller( index );
       return value;
    }
 
@@ -94,6 +92,8 @@ public class IntList implements IntListInterface {
       list.append( 13 );
       list.append( 17 );
       list.append( 19 );
+      list.insertValueAtIndex(6, 4);
+      System.out.println(list.getValueAtIndex(4));
       System.out.println( list.getValueAtIndex( 7 ) );      // should return the value 19
       System.out.println( list.removeValueAtIndex( 3 ) );   // should return the value 7
       System.out.println( list.getValueAtIndex( 3 ) );      // should return the value 11
