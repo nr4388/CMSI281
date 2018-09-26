af517daee08f6370b7ad3096db6538fd387907cb
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-09-26 12:53:54 -0700

    Edited tests for queue.

--- Classwork/Classwork05/IntQueue.java
+++ Classwork/Classwork05/IntQueue.java
@@ -12,14 +12,14 @@ public class IntQueue {
       }
 
       public int peek() {
-          if (myQueue.size == 0) {
+          if (myQueue.getSize() == 0) {
               return 0;
           } else {
               return myQueue.getIteratorAt( 0 ).getCurrentInt();     // we use the iterator
           }
       }
 
-      public int removeLastNode() {
+      public int remove() {
           return myQueue.removeAt( myQueue.getSize() - 1 );
       }
 
@@ -30,30 +30,27 @@ public class IntQueue {
       public static void main( String[] args ) {
          IntQueue testStack = new IntQueue();
          testStack.push( 19 );
-         // System.out.println(testStack.peek());
          testStack.push( 23 );
-         // System.out.println(testStack.peek());
          testStack.push( 29 );
-         // System.out.println(testStack.peek());
          testStack.push( 31 );
-         // System.out.println(testStack.peek() );
-         // System.out.println("Size: " + testStack.getSize());
-         // testStack.removeLastNode();
-         // System.out.println("SIze: " + testStack.getSize());
-         // testStack.removeLastNode();
-         // System.out.println("SIze: " + testStack.getSize());
-         // testStack.removeLastNode();
-         // System.out.println("SIze: " + testStack.getSize());
-         // System.out.println(testStack.peek() );
-         // testStack.removeLastNode();
-         // System.out.println("SIze: " + testStack.getSize());
-         // testStack.push( 37 );
-         // testStack.push( 41 );
-         // testStack.push( 43 );
-         // testStack.push( 47 );
-         // testStack.push( 51 );
-         // testStack.push( 57 );
-         System.out.println( "The top of the queue: " + testStack.peek() );      // 57
+         testStack.push( 37 );
+         testStack.push( 41 );
+         testStack.push( 43 );
+         testStack.push( 47 );
+         testStack.push( 51 );
+         testStack.push( 57 );
+
+         System.out.println("Top of queue: " + testStack.peek() );
+         System.out.println("Size of the stack is: " + testStack.getSize() );
+         System.out.println("Removed value: " + testStack.remove() );
+         System.out.println("The new size of the stack is: " + testStack.getSize() );
+
+         System.out.println("The new top of queue is: " + testStack.peek() );
+         System.out.println("Size of the stack is: " + testStack.getSize() );
+         System.out.println("Removed value: " + testStack.remove() );
+         System.out.println("The new size of the stack is: " + testStack.getSize() );
+
+         // System.out.println( "The top of the queue: " + testStack.peek() );      // 57
          // System.out.println( "Removing back thing: " + testStack.removeAt() + " removed.");         // 57 removed
          // System.out.println(testStack.getSize());
          // System.out.println( "The top of the queue: " + testStack.peek() );      // 51
