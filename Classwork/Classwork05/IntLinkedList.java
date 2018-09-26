b92ef9a4ee81ca248b9a3a5c20760f23a438d11f
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-09-26 12:46:55 -0700

    Added IntLinkedList and tester to Classwork05 folder

---
+++ Classwork/Classwork05/IntLinkedList.java
@@ -0,0 +1,124 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  IntLinkedList.java
+ *  Purpose       :  Implementing linked list manipulation to get familiar with linked lists in java.
+ *  Author        :  Nicolas Raymundo
+ *  Date          :  09-18-2018
+ *  Description   :  Implemented various methods to add and remove various nodes from a linked list.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class IntLinkedList {
+
+   private Node head;
+   public int  size;
+
+   public void insertAt(int index, int dataValue) {
+       if ((index > size - 1) || (index < 0)) {
+           throw new IllegalArgumentException("Index is either too large or too small");
+       }
+       if (index == 0) {
+           Iterator nodeTracker = getIteratorAt(index);
+           Node newNode = new Node(dataValue);
+           head = newNode;
+       } else {
+           Iterator nodeTracker = getIteratorAt(index - 1);
+           Node newNode = new Node(dataValue);
+           newNode.next = nodeTracker.currentNode.next;
+           nodeTracker.currentNode.next = newNode;
+       }
+   }
+
+   public int removeAt(int index) {
+       if (getSize() == 0) {
+           throw new IllegalArgumentException("Linked list is empty.");
+       } else if ((index > size - 1) || (index < 0)) {
+           throw new IllegalArgumentException("Index is either too large or too small");
+       }
+       if (index == 0) {
+           Iterator nodeTracker = getIteratorAt(index);
+           head = null;
+           size--;
+           return 0;
+           // return nodeTracker.getCurrentInt();
+       } else {
+           Iterator nodeTracker = getIteratorAt(index - 1);
+           nodeTracker.currentNode.next = null;
+           size--;
+           return nodeTracker.getCurrentInt();
+       }
+       // if (index == 0) {
+       //     Iterator nodeTracker = getIteratorAt(index);
+       //     head = head.next;
+       //     return nodeTracker.getCurrentInt();
+       // } else {
+       //     Iterator nodeTracker = getIteratorAt(index - 1);
+       //     nodeTracker.currentNode.next = nodeTracker.currentNode.next.next;
+       //     return nodeTracker.getCurrentInt();
+       // }
+   }
+
+   // the constructor
+   IntLinkedList() {
+      head = null;
+      size = 0;
+   }
+
+   public int getSize() {
+      return size;
+   }
+
+   public void prepend( int dataToAdd ) {
+      Node currentHead = head;
+      head = new Node( dataToAdd );
+      head.next = currentHead;
+      size++;
+   }
+
+   private class Node {
+
+      int data;            // remember this is an IntLinkedList
+      Node next;           // here's the self-referential part
+
+      // constructor
+      Node( int d ) {
+         data = d;
+         next = null;
+      }
+   }
+
+   public Iterator getIteratorAt( int index ) {
+      if( (index > size) || (index < 0) ) {
+         throw new IllegalArgumentException();
+      }
+      Iterator it = new Iterator();
+      while( index > 0 ) {
+         it.next();
+         index--;
+      }
+      return it;
+   }
+
+   public class Iterator {
+      private Node currentNode;
+
+      Iterator() {
+         currentNode = head;
+      }
+
+      public void next() {
+         if( currentNode == null ) {
+            return;
+         } else {
+            currentNode = currentNode.next;
+         }
+      }
+
+      public boolean hasNext() {
+         return ((currentNode != null) && (currentNode.next != null));
+      }
+
+      public int getCurrentInt() {
+         return currentNode.data;
+      }
+
+   }
+}
---
+++ Classwork/Classwork05/IntLinkedListTester.java
@@ -0,0 +1,49 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  IntLinkedListTester.java
+ *  Purpose       :  Tester to see if methods for linked list manipulation worked.
+ *  Author        :  Nicolas Raymundo
+ *  Date          :  09-18-2018
+ *  Description   :  Implemented tests to create a linked list and add/remove nodes after creation.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class IntLinkedListTester {
+    public static void main( String[] args ) {
+        IntLinkedList myList = new IntLinkedList();
+        myList.prepend( 23 );
+        myList.prepend( 19 );
+        myList.prepend( 17 );
+        myList.prepend( 13 );
+        myList.prepend( 11 );
+        myList.prepend(  7 );
+        myList.prepend(  5 );
+        myList.prepend(  3 );
+        myList.prepend(  2 );
+        // myList.insertAt(8, 696969);
+        myList.removeAt(2);
+        // myList.removeAt(2);
+        // myList.insertAt(5, 1414);
+        // myList.insertAt(7, 3535);
+        // myList.removeAt(4);
+        // myList.removeAt(9); // Will return an error because index 9 is not in list.
+        IntLinkedList.Iterator myIt = myList.getIteratorAt( 0 );
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 2
+        myIt.next();
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 3
+        myIt.next();
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 5
+        myIt.next();
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 7
+        myIt = myList.getIteratorAt( 3 );
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 7
+        myIt.next();
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 11
+        myIt.next();
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 13
+        myIt.next();
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );    // 17
+        myIt.next();
+        System.out.println( "Current Node is: " + myIt.getCurrentInt() );
+        // myIt.next();
+        // System.out.println( "Current Node is: " + myIt.getCurrentInt() );
+    }
+}
