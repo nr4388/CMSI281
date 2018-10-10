7d4c20a9a3560348f2288ced93ef308d39bec9b4
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-10-09 17:13:38 -0700

    Fixed formatting.

--- Homework/Homework02/Queue.java
+++ Homework/Homework02/Queue.java
@@ -1,60 +1,51 @@
-public class Queue
-{
-  private int maxSize;
-  private long[] queArray;
-  private int front;
-  private int rear;
-  private int nItems;
+public class Queue {
+    private int maxSize;
+    private long[] queArray;
+    private int front;
+    private int rear;
+    private int nItems;
 
-  public Queue(int s){ //constrcutor
-    maxSize = s;
-    queArray = new long[maxSize];
-    front = 0;
-    rear = -1;
-    nItems = 0;
-  }
-
-  public void insert(long j){          // put item at rear of queue
-    if(rear == maxSize-1) {
-      rear = -1;
+    public Queue(int s) {   //constructor
+        maxSize = s;
+        queArray = new long[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
     }
-    queArray[++rear] = j;
-    nItems++;
-  }
 
-  public long remove(){                // take item from front of queue
-    long temp = queArray[front++];
-    if(front == maxSize) {
-      front = 0;
+    public void insert(long j) {          // put item at rear of queue
+        if(rear == maxSize-1) {
+            rear = -1;
+        }
+        queArray[++rear] = j;
+        nItems++;
     }
-    nItems--;
-    return temp;
-  }
+
+    public long remove() {                // take item from front of queue
+        long temp = queArray[front++];
+        if(front == maxSize) {
+            front = 0;
+        }
+        nItems--;
+        return temp;
+    }
 
 
-  public long peekFront(){             // peek at front of queue
-    return queArray[front];
-  }
+    public long peekFront() {             // peek at front of queue
+        return queArray[front];
+    }
 
-  public boolean isEmpty(){            // true if queue is empty
-    return (nItems==0);
-  }
+    public boolean isEmpty() {            // true if queue is empty
+        return (nItems==0);
+    }
 
-  public boolean isFull() {           // true if queue is full
-    return (nItems==maxSize);
-  }
+    public boolean isFull() {           // true if queue is full
+        return (nItems==maxSize);
+    }
 
-  public int size() {                 // number of items in queue
-    return nItems;
-  }
-
-  // Note that this does not mean simply displaying the contents of the underlying array.
-  // You should show the queue contents from the first item inserted to the last,
-  // without indicating to the viewer whether the sequence is broken by wrapping around the end of the array.
-  // Be careful that one item and no items display properly, no matter where front and rear are.
-
-  // 10,20,30,40,
-  // 60,70,80,40,50
+    public int size() {                 // number of items in queue
+        return nItems;
+    }
 
   public void displayQueueContent() {
       for (int i = 0; i < nItems; i++) {
--- Homework/Homework02/QueueApp.java
+++ Homework/Homework02/QueueApp.java
@@ -1,24 +1,29 @@
 public class QueueApp {
-  public static void main(String[] args) {
-    Queue theQueue = new Queue(5);           // queue holds 5 items
-    theQueue.insert(10);
-    theQueue.insert(20);
-    theQueue.insert(30);
-    theQueue.insert(40);                     //insert 10,20,30,40
-    theQueue.remove();
-    theQueue.remove();
-    theQueue.remove();                       // remove 3 items (10, 20, 30)
-    theQueue.insert(50);
-    theQueue.insert(60);
-    theQueue.insert(70);
-    theQueue.insert(80);                     // insert 4 more items (wraps arounds)
-    theQueue.displayQueueContent();          // 40 50 60 70 80
-    System.out.println( " ");
-    while( !theQueue.isEmpty() ) {           //remove and display all items
-      long n = theQueue.remove();            // (40, 50, 60, 70, 80)
-      System.out.print(n);
-      System.out.print(" ");
-    }
-    System.out.println("");
+    public static void main(String[] args) {
+        Queue theQueue = new Queue(10);           // queue holds 5 items
+        theQueue.insert(10);
+        theQueue.insert(20);
+        theQueue.insert(30);
+        theQueue.insert(40);                     //insert 10,20,30,40
+        theQueue.insert(50);
+        theQueue.insert(60);
+        theQueue.insert(70);
+        theQueue.insert(80);
+        theQueue.insert(90);
+        theQueue.remove();
+        theQueue.remove();
+        theQueue.remove();                       // remove 3 items (10, 20, 30)
+        theQueue.insert(100);
+        theQueue.insert(110);
+        theQueue.insert(120);
+        theQueue.insert(130);                     // insert 4 more items (wraps arounds)
+        theQueue.displayQueueContent();          // 40 50 60 70 80 90 100 110 120 130
+        System.out.println( " ");
+        while( !theQueue.isEmpty() ) {           //remove and display all items
+            long n = theQueue.remove();            // (40, 50, 60, 70, 80, 90, 100, 110, 120, 130)
+            System.out.print(n);
+            System.out.print(" ");
+        }
+        System.out.println("");
     }
 }
