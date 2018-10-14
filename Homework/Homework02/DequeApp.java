843f0211f804dbe7f760fc1d2abc0192ae4fbc6e
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-10-14 16:08:52 -0700

    Added tests for isFull and isEmpty. Both pass all tests.

--- Homework/Homework02/DequeApp.java
+++ Homework/Homework02/DequeApp.java
@@ -2,10 +2,18 @@ public class DequeApp {
     public static void main(String[] args) {
         Deque theDeque = new Deque(4);           // queue holds 4 items
 
+        // System.out.println("Is the deque empty? " + theDeque.isEmpty());    // True
+
         theDeque.insertLeft(12);
         theDeque.insertRight(13);
         theDeque.insertLeft(11);
+
+        // System.out.println("Is the deque full? " + theDeque.isFull());  // false
+
         theDeque.insertRight(14);   // 11, 12, 13, 14
+
+        // System.out.println("Is the deque full? " + theDeque.isFull());  // true
+
         theDeque.insertRight(15);   // 12, 13, 14, 14
         theDeque.insertRight(16);   // 13, 14, 15, 16
         theDeque.insertLeft(12);    // 12, 13, 14, 15
@@ -44,7 +52,9 @@ public class DequeApp {
         theDeque.insertLeft(11);
         theDeque.insertRight(14);   // 11, 12, 13, 14
 
-        // System.out.println( " ");
+        System.out.print("Is the deque full? " + theDeque.isFull());
+
+        System.out.println( " ");
         while( !theDeque.isEmpty() ) {           //remove and display all items
             long n = theDeque.removeLeft();
             System.out.print(n);
