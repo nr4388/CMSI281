1902eda0de165ba2e0fd5714379d1bdbbe0c2122
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-10-15 15:46:45 -0700

    Edited test comments

--- Homework/Homework02/QueueApp.java
+++ Homework/Homework02/QueueApp.java
@@ -4,15 +4,17 @@ public class QueueApp {
         theQueue.insert(10);
         theQueue.insert(20);
         theQueue.insert(30);
-        theQueue.insert(40);                     //insert 10,20,30,40
+        theQueue.insert(40);
         theQueue.insert(50);
         theQueue.insert(60);
         theQueue.insert(70);
         theQueue.insert(80);
-        theQueue.insert(90);
+        theQueue.insert(90);                     //insert 10, 20, 30, 40, 50, 60, 70, 80, 90
+
         theQueue.remove();
         theQueue.remove();
         theQueue.remove();                       // remove 3 items (10, 20, 30)
+
         theQueue.insert(100);
         theQueue.insert(110);
         theQueue.insert(120);
