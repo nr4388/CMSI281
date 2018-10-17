d141d16e27d4e78612474d71e1817ee20dc1cc7b
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-10-16 19:04:51 -0700

    Added tests for new wraparound. Currently all passing.

--- Homework/Homework02/DequeApp.java
+++ Homework/Homework02/DequeApp.java
@@ -2,67 +2,67 @@ public class DequeApp {
     public static void main(String[] args) {
         Deque theDeque = new Deque(4);           // queue holds 4 items
 
-        // System.out.println("Is the deque empty? " + theDeque.isEmpty());    // True
-
         theDeque.insertLeft(12);
-        theDeque.insertRight(13);
+        theDeque.insertLeft(13);
         theDeque.insertLeft(11);
+        theDeque.insertLeft(98);                // 98, 11, 13, 12
 
-        // System.out.println("Is the deque full? " + theDeque.isFull());  // false
+        System.out.println("Starting " + theDeque.displayDequeContent());     // 98, 11, 13, 12
 
-        theDeque.insertRight(14);   // 11, 12, 13, 14
+        System.out.println("\n");
 
-        // System.out.println("Is the deque full? " + theDeque.isFull());  // true
+        System.out.println("Inserting right. Expected: Deque is full.");
+        theDeque.insertRight(12);               // Deque is full
 
-        theDeque.insertRight(15);   // 12, 13, 14, 14
-        theDeque.insertRight(16);   // 13, 14, 15, 16
-        theDeque.insertLeft(12);    // 12, 13, 14, 15
-        theDeque.insertLeft(11);    // 11, 12, 13, 14
-        theDeque.insertLeft(10);    // 10, 11, 12, 13
-        theDeque.insertRight(14);   // 11, 12, 13, 14
-        theDeque.insertRight(15);   // 12, 13, 14, 15
+        System.out.println("\n");
 
-        theDeque.removeRight(); // 12, 13, 14
+        System.out.println("Removing right. Expected: 98 11 13");
+        theDeque.removeRight();
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // 98 11 13
 
-        theDeque.insertRight(15);   // 12, 13, 14, 15
+        System.out.println("Inserting left. Expected: 98 11 13 76");
+        theDeque.insertRight(76);               // 98 11 13 76
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // 98 11 13 76
 
-        theDeque.removeLeft();  // 13, 14, 15
+        System.out.println("Inserting left. Expected: Deque is full.");
+        theDeque.insertLeft(12);               // Deque is full
 
-        theDeque.insertLeft(12);    // 12, 13, 14, 15
-        theDeque.insertRight(16);   // 13, 14, 15 ,16
-        theDeque.insertLeft(12);    // 12, 13, 14, 15
-        theDeque.insertLeft(11);    // 11, 12, 13, 14
-        theDeque.insertLeft(10);    // 10, 11, 12, 13
-        theDeque.insertRight(14);   // 11, 12, 13, 14
-        theDeque.insertLeft(10);    // 10, 11, 12, 13
+        System.out.println("\n");
 
-        theDeque.removeLeft();  // 11, 12, 13
-        theDeque.removeLeft();  // 12, 13
+        System.out.println("Clearing array. Expected: Empty deque");
+        theDeque.removeRight();
+        theDeque.removeRight();
+        theDeque.removeRight();
+        theDeque.removeRight();
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // Empty deque
 
-        theDeque.insertLeft(11);    // 11, 12, 13
-        theDeque.insertLeft(10);    // 10, 11, 12, 13
+        System.out.println("Creating deque by interchanging insertRight and insertLeft. Expected: 99 14 32 42");
+        theDeque.insertLeft(14);
+        theDeque.insertRight(32);
+        theDeque.insertLeft(99);
+        theDeque.insertRight(42);
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // 99 14 32 42
 
-        theDeque.removeLeft();  // 11, 12, 13
-        theDeque.removeLeft();  // 12, 13
-        theDeque.removeLeft();  // 13
-        theDeque.removeLeft(); // empty deque
+        System.out.println("Inserting left. Expected: Deque is full.");
+        theDeque.insertLeft(87);
 
-        theDeque.insertLeft(12);
-        theDeque.insertRight(13);
-        theDeque.insertLeft(11);
-        theDeque.insertRight(14);   // 11, 12, 13, 14
+        System.out.println("\n");
 
-        System.out.println("Is the deque empty? " + theDeque.isEmpty());  // False
+        System.out.println("Removing left. Expected: 14 32 42");
+        theDeque.removeLeft();
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // 14 32 42
 
-        System.out.print("Is the deque full? " + theDeque.isFull());    // true
+        System.out.println("Removing right. Expected: 14 32");
+        theDeque.removeRight();
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // 14 32
 
-        System.out.println( " ");
-        while( !theDeque.isEmpty() ) {           //remove and display all items
-            long n = theDeque.removeLeft();
-            System.out.print(n);
-            System.out.print(" ");
-        }
-        System.out.println("");
+        System.out.println("Inserting left. Expected: 52 14 32");
+        theDeque.insertRight(52);               // 98 11 13 76
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // 52 14 32
+
+        System.out.println("Inserting left. Expected: 79 52 14 32");
+        theDeque.insertRight(79);               // 98 11 13 76
+        System.out.println(theDeque.displayDequeContent()+ "\n");       // 79 52 14 32
 
     }
 }
