c88a6338b934a9b635ba51c337c2b35905c27bb5
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-10-16 19:07:03 -0700

    Added tests for new remove method

--- Homework/Homework02/QueueApp.java
+++ Homework/Homework02/QueueApp.java
@@ -1,25 +1,30 @@
 public class QueueApp {
     public static void main(String[] args) {
-        Queue theQueue = new Queue(10);           // queue holds 10 items
+        Queue theQueue = new Queue(5);           // queue holds 5 items
+
+        System.out.println("Inserting 10, 20, 30, 40, 50");
+
         theQueue.insert(10);
         theQueue.insert(20);
         theQueue.insert(30);
         theQueue.insert(40);
-        theQueue.insert(50);
-        theQueue.insert(60);
-        theQueue.insert(70);
-        theQueue.insert(80);
-        theQueue.insert(90);                     //insert 10, 20, 30, 40, 50, 60, 70, 80, 90
+        theQueue.insert(50);                     //insert 10, 20, 30, 40, 50
+
+        theQueue.displayQueueContent();
+
+        System.out.println("\n" + "Removing 10, 20, 30");
 
         theQueue.remove();
         theQueue.remove();
         theQueue.remove();                       // remove 3 items (10, 20, 30)
+        theQueue.displayQueueContent();          // 40, 50
 
-        theQueue.insert(100);
-        theQueue.insert(110);
-        theQueue.insert(120);
-        theQueue.insert(130);                     // insert 4 more items (wraps arounds)
-        theQueue.displayQueueContent();          // 40 50 60 70 80 90 100 110 120 130
+        System.out.println("\n" + "Inserting 60, 70, 80");
+
+        theQueue.insert(60);
+        theQueue.insert(70);
+        theQueue.insert(80);                     // insert 2 more items
+        theQueue.displayQueueContent();          // 40 50 60 70
         System.out.println( " ");
         while( !theQueue.isEmpty() ) {           //remove and display all items
             long n = theQueue.remove();            // (40, 50, 60, 70, 80, 90, 100, 110, 120, 130)
