96645361b0d478998e8a1156979d82aa8b376bbf
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-10-17 15:34:27 -0700

    Edited comments format

--- Homework/Homework02/StackApp.java
+++ Homework/Homework02/StackApp.java
@@ -10,42 +10,42 @@
     public static void main(String[] args) {
         Stack theList = new Stack();
 
-        theList.push(11);   // 11
-        theList.push(33);   // 33, 11
-        theList.push(55);   // 55, 33, 11
-        theList.push(99);   // 99, 55, 33, 11
+        theList.push(11);                   // 11
+        theList.push(33);                   // 33, 11
+        theList.push(55);                   // 55, 33, 11
+        theList.push(99);                   // 99, 55, 33, 11
 
-        theList.displayList();  // Print: 99 55 33 11
+        theList.displayList();              // Print: 99 55 33 11
 
-        theList.peek(); // Print: "Top of the stack: 99"
+        theList.peek();                     // Print: "Top of the stack: 99"
 
-        theList.pop();  // 55, 33, 11
-        theList.pop();  // 33, 11
-        theList.pop();  // 11
-        theList.pop();  // Empty list
-        // theList.pop();   // IllegalArgumentException: Stack is empty
+        theList.pop();                      // 55, 33, 11
+        theList.pop();                      // 33, 11
+        theList.pop();                      // 11
+        theList.pop();                      // Empty list
+        // theList.pop();                   // IllegalArgumentException: Stack is empty
 
-        // theList.peek(); // IllegalArgumentException: Stack is empty
+        // theList.peek();                  // IllegalArgumentException: Stack is empty
 
-        theList.push(2);   // 2
-        theList.push(21);   // 21, 2
-        theList.push(89);   // 89, 21, 2
-        theList.push(17);   // 17, 89, 21, 2
+        theList.push(2);                    // 2
+        theList.push(21);                   // 21, 2
+        theList.push(89);                   // 89, 21, 2
+        theList.push(17);                   // 17, 89, 21, 2
 
-        theList.displayList();  // Print: 17 89 21 2
+        theList.displayList();              // Print: 17 89 21 2
 
-        theList.peek(); // Print: "Top of the stack: 17"
+        theList.peek();                     // Print: "Top of the stack: 17"
 
-        theList.pop();  // 89, 21, 2
+        theList.pop();                      // 89, 21, 2
 
-        theList.displayList();  // Print: 89 21 2
+        theList.displayList();              // Print: 89 21 2
 
-        theList.peek(); // Print: "Top of the stack: 89"
+        theList.peek();                     // Print: "Top of the stack: 89"
 
-        theList.push(902); // 902, 89, 21, 2
+        theList.push(902);                  // 902, 89, 21, 2
 
-        theList.displayList();  // Print: 902 89 21 2
+        theList.displayList();              // Print: 902 89 21 2
 
-        theList.peek(); // Print: "Top of the stack: 902"
+        theList.peek();                     // Print: "Top of the stack: 902"
     }
 }
