6eb2ab67274357a956edb4328a4f2b81c0aa2080
Nicolas Raymundo <nico@Nicos-MacBook-Pro.local>
2018-10-17 14:35:17 -0700

    Added tests and header.

--- Homework/Homework02/JosephusTester.java
+++ Homework/Homework02/JosephusTester.java
@@ -1,43 +1,34 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  JosephusTester.java
+ *  Purpose       :  Tester file for Josephus file.
+ *  Author        :  Nicolas Raymundo
+ *  Date          :  10-17-2018
+ *  Description   :  Calling elimination method to test whether the method properly does the Josephus Problem.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 public class JosephusTester {
     public static void main( String[] args ) {
         Josephus myList = new Josephus();
-        myList.insert( 1 );
-        myList.insert( 2 );
-        myList.insert( 3 );
-        myList.insert( 4 );
 
-        System.out.println(myList.suicideCircle(4, 3, 2));
+        /** ------------------------------------------------------------
+         My Josephus method works like this:
+         Given a circle up to 4, eliminating every 2 and starting at 1:
+         1 2 3 4
+         1 0 3 4
+         1 0 3 0
+         1 0 0 0
+         Eliminated in order: 2 4 3.
+         Survivor is 1.
+        ------------------------------------------------------------- */
 
-        // myList.deleteAt(1);
-        // myList.deleteAt(1);
+        System.out.println(myList.elimination(9, 4) + "\n");            // 4 8 3 9 6 5 7 2   Survivor: 1
 
-        // System.out.println(myList.search(0));
-        // System.out.println(myList.search(1));
-        // System.out.println(myList.search(2));
-        // System.out.println(myList.search(3));
+        System.out.println(myList.elimination(4, 2) + "\n");            // 2 4 3 Survivor: 1
 
-        // System.out.println(myList.search(7));
-        // System.out.println(myList.search(8));
-        // System.out.println(myList.search(9));
-        // System.out.println(myList.search(10));
-        // System.out.println(myList.search(11));
-        // System.out.println(myList.search(12));
-        // System.out.println(myList.search(13));
-        // System.out.println(myList.search(14));
+        System.out.println(myList.elimination(9, 7) + "\n");            // 7 5 4 6 9 3 8 1 Survivor: 2
 
-        // myList.suicideCircle(7, 4, 1);
+        System.out.println(myList.elimination(15, 3) + "\n");            // 3 6 9 12 15 4 8 13 2 10 1 11 7 14 Survivor: 5
 
-        // Josephus.Iterator myIt = myList.getIteratorAt( 0 );
-        // System.out.println( "Current Node is: " + myIt.getCurrentInt() );   // 23
-        // myIt.step();
-        // System.out.println( "Current Node is: " + myIt.getCurrentInt() );   // 12
-        // myIt.step();
-        // System.out.println( "Current Node is: " + myIt.getCurrentInt() );   // 99
-        // myIt.step();
-        // System.out.println( "Current Node is: " + myIt.getCurrentInt() );   // 99
-        // myIt.step();
-        // System.out.println( "Current Node is: " + myIt.getCurrentInt() );   // 99
-        // myIt.step();
-        // System.out.println( "Current Node is: " + myIt.getCurrentInt() );   // 99
+        System.out.println(myList.elimination(24, 9) + "\n");            // 9 18 3 13 23 10 21 8 22 12 2 17 11 5 1 24 4 7 16 14 15 6 19 Survivor: 20
     }
 }
